dc797b738b88bb5351dc202df5ded4b60a0a3ff6
Thomas <me@hvs.cx>
2021-06-14 00:22:15 +0200

    make navbar disclosure

--- src/Partials/Navbar.js
+++ src/Partials/Navbar.js
@@ -1,14 +1,14 @@
 import {Fragment} from "react";
-import {Popover, Transition} from "@headlessui/react";
+import {Disclosure, Transition} from "@headlessui/react";
 import Logo from "../Svg/Logo";
 import {MenuIcon, XIcon} from "@heroicons/react/outline";
 
 export default function Navbar() {
     return (
-        <Popover>
+        <Disclosure as="div">
             {({ open }) => (
                 <>
-                    <div className="max-w-7xl mx-auto px-4 sm:px-6">
+                    <div className="max-w-7xl mx-auto px-4 sm:px-6 md:pb-12 lg:pb-16">
                         <nav
                             className="relative flex items-center justify-between sm:h-10 md:justify-center"
                             aria-label="Global"
@@ -18,17 +18,17 @@ export default function Navbar() {
                                     <Logo className="h-16 w-auto" />
 
                                     <div className="-mr-2 flex items-center md:hidden">
-                                        <Popover.Button className="bg-gray-50 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                        <Disclosure.Button className="bg-gray-50 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                                             <span className="sr-only">Open main menu</span>
                                             <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                                        </Popover.Button>
+                                        </Disclosure.Button>
                                     </div>
                                 </div>
                             </div>
-                            <div className="hidden md:absolute md:flex md:items-center md:justify-end md:inset-y-0 md:right-0">
+                            <div className="hidden md:absolute md:flex md:items-center md:justify-end md:inset-y-0 md:right-0 space-x-8">
                                 <a
                                     href="#"
-                                    className="inline-block py-2 px-4 border border-transparent rounded-md text-base font-medium text-black hover:text-gray-800"
+                                    className="small-border inline-block text-base font-bold text-black hover:text-gray-800"
                                 >
                                     Home
                                 </a>
@@ -46,50 +46,38 @@ export default function Navbar() {
                     <Transition
                         show={open}
                         as={Fragment}
-                        enter="duration-150 ease-out"
-                        enterFrom="opacity-0 scale-95"
-                        enterTo="opacity-100 scale-100"
-                        leave="duration-100 ease-in"
-                        leaveFrom="opacity-100 scale-100"
-                        leaveTo="opacity-0 scale-95"
+                        enter="transition duration-100 ease-out"
+                        enterFrom="transform scale-95 opacity-0"
+                        enterTo="transform scale-100 opacity-100"
+                        leave="transition duration-75 ease-out"
+                        leaveFrom="transform scale-100 opacity-100"
+                        leaveTo="transform scale-95 opacity-0"
                     >
-                        <Popover.Panel
-                            focus
+                        <Disclosure.Panel
                             static
-                            className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+                            className="md:hidden"
                         >
-                            <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                                <div className="px-5 pt-4 flex items-center justify-between">
-                                    <div>
-                                        <Logo className="h-16 w-auto" />
-                                    </div>
-                                    <div className="-mr-2">
-                                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                                            <span className="sr-only">Close menu</span>
-                                            <XIcon className="h-6 w-6" aria-hidden="true" />
-                                        </Popover.Button>
-                                    </div>
-                                </div>
-                                <div className="px-2 pt-2 pb-3 flex flex-col justify-center items-center">
+                            <div className="overflow-hidden">
+                                <div className="px-2 pt-2 pb-3 flex flex-col justify-center items-center space-y-4">
                                     <a
                                         href="#"
-                                        className="inline-block py-2 px-4 border border-transparent rounded-md text-base font-medium text-black hover:text-gray-800"
+                                        className="inline-block text-base font-medium text-black hover:text-gray-800"
                                     >
                                         Home
                                     </a>
 
                                     <a
                                         href="#"
-                                        className="inline-block py-1 px-5 border border-transparent rounded-xl text-base font-medium text-maldive transition duration-200 ease-in-out"
+                                        className="inline-block rounded-xl text-base font-medium text-maldive transition duration-200 ease-in-out"
                                     >
                                         Submit A Flip
                                     </a>
                                 </div>
                             </div>
-                        </Popover.Panel>
+                        </Disclosure.Panel>
                     </Transition>
                 </>
             )}
-        </Popover>
+        </Disclosure>
     )
 }
